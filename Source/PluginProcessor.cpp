40c9c58282899cbbc110bea5119ba305e5b64722
FlorisLeirs <floris.leirs@gmail.com>
2023-04-25 11:08:11 +0200

    Parameter layout created

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin processor.
+	This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -12,15 +12,16 @@
 //==============================================================================
 Fader_RiderAudioProcessor::Fader_RiderAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+	: AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+		.withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
+		.withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+	)
+#endif
+	, valueTreeState(*this, nullptr, "Parameters", CreateParameterLayout())
 {
 }
 
@@ -31,161 +32,197 @@ Fader_RiderAudioProcessor::~Fader_RiderAudioProcessor()
 //==============================================================================
 const juce::String Fader_RiderAudioProcessor::getName() const
 {
-    return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool Fader_RiderAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_WantsMidiInput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool Fader_RiderAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_ProducesMidiOutput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool Fader_RiderAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_IsMidiEffect
+	return true;
+#else
+	return false;
+#endif
 }
 
 double Fader_RiderAudioProcessor::getTailLengthSeconds() const
 {
-    return 0.0;
+	return 0.0;
 }
 
 int Fader_RiderAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+	return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+	// so this should be at least 1, even if you're not really implementing programs.
 }
 
 int Fader_RiderAudioProcessor::getCurrentProgram()
 {
-    return 0;
+	return 0;
 }
 
-void Fader_RiderAudioProcessor::setCurrentProgram (int index)
+void Fader_RiderAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String Fader_RiderAudioProcessor::getProgramName (int index)
+const juce::String Fader_RiderAudioProcessor::getProgramName(int index)
 {
-    return {};
+	return {};
 }
 
-void Fader_RiderAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void Fader_RiderAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void Fader_RiderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void Fader_RiderAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+	// Use this method as the place to do any pre-playback
+	// initialisation that you need..
 }
 
 void Fader_RiderAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+	// When playback stops, you can use this as an opportunity to free up any
+	// spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool Fader_RiderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool Fader_RiderAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+#if JucePlugin_IsMidiEffect
+	juce::ignoreUnused(layouts);
+	return true;
+#else
+	// This is the place where you check if the layout is supported.
+	// In this template code we only support mono or stereo.
+	// Some plugin hosts, such as certain GarageBand versions, will only
+	// load plugins that support stereo bus layouts.
+	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+		&& layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+		return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+	// This checks if the input layout matches the output layout
+#if ! JucePlugin_IsSynth
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
+#endif
 
-    return true;
-  #endif
+	return true;
+#endif
 }
 #endif
 
-void Fader_RiderAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void Fader_RiderAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+	juce::ScopedNoDenormals noDenormals;
+	auto totalNumInputChannels = getTotalNumInputChannels();
+	auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+	// In case we have more outputs than inputs, this code clears any output
+	// channels that didn't contain input data, (because these aren't
+	// guaranteed to be empty - they may contain garbage).
+	// This is here to avoid people getting screaming feedback
+	// when they first compile a plugin, but obviously you don't need to keep
+	// this code if your algorithm always overwrites all the output channels.
+	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+		buffer.clear(i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+	// This is the place where you'd normally do the guts of your plugin's
+	// audio processing...
+	// Make sure to reset the state if your inner loop is processing
+	// the samples and the outer loop is handling the channels.
+	// Alternatively, you can process the samples with the channels
+	// interleaved by keeping the same state.
+	for (int channel = 0; channel < totalNumInputChannels; ++channel)
+	{
+		auto* channelData = buffer.getWritePointer(channel);
 
-        // ..do something to the data...
-    }
+		// ..do something to the data...
+	}
 }
 
 //==============================================================================
 bool Fader_RiderAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+	return true; // (change this to false if you choose to not supply an editor)
 }
 
 juce::AudioProcessorEditor* Fader_RiderAudioProcessor::createEditor()
 {
-    return new Fader_RiderAudioProcessorEditor (*this);
+	return new Fader_RiderAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void Fader_RiderAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void Fader_RiderAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+	// You should use this method to store your parameters in the memory block.
+	// You could do that either as raw data, or use the XML or ValueTree classes
+	// as intermediaries to make it easy to save and load complex data.
 }
 
-void Fader_RiderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void Fader_RiderAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+	// You should use this method to restore your parameters from this memory block,
+	// whose contents will have been created by the getStateInformation() call.
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout Fader_RiderAudioProcessor::CreateParameterLayout()
+{
+	juce::AudioProcessorValueTreeState::ParameterLayout layout{};
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("TargetLevel", "TargetLevel",
+		juce::NormalisableRange<float>(-30.f, 0.f, 0.2f, 1.f),
+		0.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("FaderLevel", "FaderLevel",
+		juce::NormalisableRange<float>(-12.f, 12.f, 0.2f, 1.f),
+		0.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("RangeMax", "RangeMax",
+		juce::NormalisableRange<float>(1 - 2.f, 12.f, 0.2f, 1.f),
+		12.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("RangeMin", "RangeMin",
+		juce::NormalisableRange<float>(-12.f, 12.f, 0.2f, 1.f),
+		-12.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("VocalSensitivity", "VocalSensitivity",
+		juce::NormalisableRange<float>(-30.f, -10.f, 0.2f, 1.f),
+		-20.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("MusicSensitivity", "MusicSensitivity",
+		juce::NormalisableRange<float>(-30.f, -10.f, 0.2f, 1.f),
+		-20.f));
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>("Output", "Output",
+		juce::NormalisableRange<float>(-24.f, 24.f, 0.2f, 1.f),
+		0.f));
+
+
+	return layout;
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new Fader_RiderAudioProcessor();
+	return new Fader_RiderAudioProcessor();
 }
